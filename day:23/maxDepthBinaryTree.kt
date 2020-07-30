af38b6249dfcaa678561d50aaee5a1a2a6baf146
Mayengbam Sushil K <hawaijar.js@gmail.com>
2020-07-30 08:04:14 +0530

    Create maxDepthBinaryTree.kt
    Added Kotlin solution (maxDepthBinaryTree)

---
+++ day:23/maxDepthBinaryTree.kt
@@ -0,0 +1,18 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        // base case
+        if(root == null) return 0;
+    
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
