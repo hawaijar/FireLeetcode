0e715767f96fcbac88d6e3a3435352c164a4841b
Mayengbam Sushil K <hawaijar.js@gmail.com>
2020-08-17 14:05:03 +0530

    Create titleToNumber.js
    Added JS solution - convert Excel Ttitle to Column

---
+++ day:41/titleToNumber.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+    
+    // formula: base(26) decimal conversion.
+    
+    s = s.toUpperCase();
+    const hash = {
+        'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11,
+        'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21,
+        'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26
+    }
+    
+    // base case
+    if(s.length === 1) return hash[s[0]];
+    
+    let result = 0;
+    const size = s.length - 1;
+    
+    for(let i = size; i >= 0; i--) {
+        const char = s[i];
+        result += Math.pow(26, size - i) * hash[char];
+    }
+    
+    return result;
+    
+};
