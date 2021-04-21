e23da4b10b5e78fdf915796ff6631a515737ff32
Mayengbam Sushil K <hawaijar.js@gmail.com>
2021-04-21 11:30:46 +0530

    Update jumpII.js

--- day:114/jumpII.js
+++ day:114/jumpII.js
@@ -1,18 +1,26 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var jump = function(nums) {
-    let farthest = 0;
+function jump(nums) {
+	let farthest; // tracks the furthest jump from the possible jump positions
 	let jump = 0;
-	let lastJumpIndex = 0;
-
-	for(let i = 0; i < nums.length - 1; i++) {
-		farthest = Math.max(farthest, i + nums[i]);
-		if(i === lastJumpIndex) {
-			jump += 1;
-			lastJumpIndex = farthest;
+	let nextJumpPosition = 0; // the next position that gives the furthest jump
+	let i = 0;
+	while (i < nums.length - 1) {
+		farthest = i + nums[i];
+		if(farthest >= nums.length - 1) {
+			return ++jump;
 		}
+		let start = i + 1; // first possible jump position
+		let end = i + nums[i]; // last possible jump position
+		// find the farthest in the range (start, end) and jump into it.
+		for (let j = start; j <= end; j++) {
+			if (j + nums[j] >= farthest) {
+				farthest = j + nums[j];
+				nextJumpPosition = j;
+			}
+		}
+		jump += 1;
+		i = nextJumpPosition;
 	}
 	return jump;
-};
+}
+
+console.log(jump([7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]));
