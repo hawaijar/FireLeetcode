ff48fa549edf6ff788eff424c699aa7cf14c11f6
Mayengbam Sushil K <hawaijar.js@gmail.com>
2021-11-23 21:44:36 +0530

    Create index.swift
    Added solution(s) for TwoSum problem in Swift

---
+++ Arrays/TwoSum/index.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+class TwoNumberSum {
+  // using hash structure
+  func solution1(_ array: inout [Int], _ targetSum: Int) -> [Int] {
+    var hash: [Int: Bool] = [:]
+    
+    for num in array {
+        let a = num
+        let diff = targetSum - a
+        if let _ = hash[a] {
+            return [a, diff]
+        }
+        else {
+            hash[diff] = true
+        }
+    }
+    
+    return []
+  }
+    
+  // 2-pointers approach
+  func solution2(_ array: inout [Int], _ targetSum: Int) -> [Int] {
+      // base case(s)
+      if array.count < 2 {
+          return []
+      }
+      // sort the array first
+      array.sort()
+      var i = 0, j = array.count - 1
+      
+      while i < j {
+          let a: Int = array[i]
+          let b: Int = array[j]
+          
+          if(a + b == targetSum) {
+              return [a, b]
+          }
+          if(a + b < targetSum) {
+              i += 1
+          }
+          else {
+              j -= 1
+          }
+      }
+      
+      return []
+    }
+  }
