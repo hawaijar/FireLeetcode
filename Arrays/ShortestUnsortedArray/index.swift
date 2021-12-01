9090db2cd5af2ae08467da122686d2d501d152eb
Mayengbam Sushil K <hawaijar.js@gmail.com>
2021-12-01 08:51:48 +0530

    Update index.swift
    Added Solution(2) for shortestUnsortedArray

--- Arrays/ShortestUnsortedArray/index.swift
+++ Arrays/ShortestUnsortedArray/index.swift
@@ -28,6 +28,71 @@ class SubArraySort {
         }
         return result
     }
+    
+    // scanning approach
+    func solution2(array: [Int]) -> [Int] {
+        // base case(s)
+        if array.count == 0 || array.count < 2 {
+            return [-1,-1]
+        }
+        // Step1: find all the unsorted elements
+        var unsorted: [Int] = []
+        
+        for (i, _) in array.enumerated() {
+            let current = array[i]
+            if i == 0 {
+                if current > array[i + 1] {
+                    unsorted.append(current)
+                }
+            }
+            else if i == array.count - 1 {
+                if current < array[i - 1] {
+                    unsorted.append(current)
+                }
+            }
+            else {
+                let previous = array[i - 1]
+                let next = array[i + 1]
+                
+                if current < previous || current > next {
+                    unsorted.append(current)
+                }
+            }
+        }
+        
+        // Step2: find the smallest and largest from the unsorted array
+        
+        var smallest = Int.max
+        var largest = Int.min
+        
+        for num in unsorted {
+            smallest = min(smallest, num)
+            largest = max(largest, num)
+        }
+        
+        // Step3: Find the positions (i, j) of smallest and largest in the the array
+        
+        var result: [Int] = [-1, -1]
+        
+        for (i, num) in array.enumerated() {
+            if smallest < num {
+                result[0] = i
+                break
+            }
+        }
+        
+        var j = array.count - 1
+        
+        while j >= 0 {
+            if largest > array[j] {
+                result[1] = j
+                break
+            }
+            j -= 1
+        }
+        
+       return result
+    }
 }
 
 // let array = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
