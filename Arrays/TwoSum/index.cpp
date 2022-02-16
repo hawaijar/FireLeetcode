2364c3e5427e2f28b50516e6327caf14d3c8c1f1
Mayengbam Sushil K <hawaijar.js@gmail.com>
2022-02-16 20:25:08 +0530

    Update index.cpp

--- Arrays/TwoSum/index.cpp
+++ Arrays/TwoSum/index.cpp
@@ -8,12 +8,14 @@ using namespace std;
 class TwoSumSolution {
 public:
     vector<int> solution1(vector<int> &nums, int target);
+    vector<int> solution2(vector<int> &nums, int target);
 };
 
 #endif //FIRELEETCODE_CPP_TWOSUM_H
 
 #include <map>
 
+// hash/map based solution
 vector<int> TwoSumSolution::solution1(vector<int> &nums, int target) {
    if(nums.size() < 2) {
        return vector<int>{-1, -1};
@@ -37,4 +39,38 @@ vector<int> TwoSumSolution::solution1(vector<int> &nums, int target) {
        }
    }
     return result;
+}
+
+// Two/pointers solution
+struct Data {
+    int data;
+    int index;
+};
+bool valueCmp(const struct Data& one, const struct Data& two) {
+    return one.data < two.data;
+}
+vector<int> TwoSumSolution::solution2(vector<int> &nums, int target) {
+    if(nums.size() < 2) {
+        return vector<int>{-1, -1};
+    }
+    vector<Data> list;
+    for(int i = 0; i < nums.size(); i++) {
+        list.push_back({nums[i], i});
+    }
+    // sort the list (of structures)
+    sort(list.begin(), list.end(), valueCmp);
+    int i = 0, j = nums.size() - 1;
+    while(i < j) {
+        int sum = list[i].data + list[j].data;
+        if(sum == target) {
+            return vector<int>{list[i].index, list[j].index};
+        }
+        else if(sum < target) {
+            i += 1;
+        }
+        else {
+            j -= 1;
+        }
+    }
+    return vector<int>{-1, -1};
 }
