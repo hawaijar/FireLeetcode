fb0fe026c2c2b382983d5a31e6f097cd32f8fcec
hawaijar <hawaijar.js@gmail.com>
2022-04-06 10:52:50 +0530

    Added Swift solution for validateSequence

---
+++ Arrays/ValidateSequence/index.swift
@@ -0,0 +1,22 @@
+func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
+    // base case
+    if sequence.count == 0 || array.count == 0 {
+        return false
+    }
+    var i = 0
+    var j = 0
+    
+    while i < array.count {
+        if(array[i] == sequence[j]) {
+            j += 1
+            i += 1
+            if j == sequence.count {
+                return true
+            }
+        }
+        else {
+            i += 1
+        }
+    }
+    return false
+}
