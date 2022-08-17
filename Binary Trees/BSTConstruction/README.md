57f3f545b82aef3a3311bff53bedbe9f4854b925
hawaijar <hawaijar.js@gmail.com>
2022-08-17 09:59:28 +0530

    feat: Updated the Binary Search Tree README.md

--- Binary Trees/BSTConstruction/README.md
+++ Binary Trees/BSTConstruction/README.md
@@ -7,7 +7,69 @@ Write a Binary Search Tree that supports the following operations -
 
 `remove(value) - remove a value`
 
+### Solution:
 
-#### One key point to be noted:
+First thing first - Let's create a BST Node as shown below -
+```
+class BSTNode {
+    value: number;
+    left: BSTNode|null;
+    right: BSTNode|null;
+}
+```
+In the above, the *left* and *right* child could be **null** and hence the null notation there.
 
-- For the remove(), consider the case where the node to be deleted is a **root node,** and it's the **only node** in the tree.
+In the Binary Search Tree, the **hardest part** is the **remove operation** and therefore we'll deal it at the last.
+
+#### Insert operation
+Algorithm for _Insert_ operation is simple -
+
+* Identify which subtree (left or right) does the value to be inserted belongs to.
+  * If it's a left/subtree: 
+    * If the left/subtree is empty, `node.left = new BSTNode(value) `
+    ![img.png](img.png)
+    * Else, `node.left.insert(value)`
+  * If it's a right/subtree:
+      * If the right/subtree is empty, `node.right = new BSTNode(value) `
+      ![img_1.png](img_1.png)
+      * Else, `node.right.insert(value)`
+
+#### Contains (or Find) operation
+This is similar to the _Insert operation_ in that when the _left_ or _right_ child is empty, we return _false_ (instead of creating new Node)
+* Identify which subtree (left or right) does the value to be inserted belongs to.
+  * If it's a left/subtree:
+    * If the left/subtree is empty, `return false`
+    * Else, `return node.left.contains(value)`
+  * If it's a right/subtree:
+    * If the right/subtree is empty, `return false`
+    * Else, `return node.right.contains(value)`
+
+#### Remove operation
+Now the _bread and butter_ of BST Algorithm
+
+For the _remove operation_, **we would need a Parent node** for each node element. This will become clear when we explain the algorithm.
+
+**Case 1:** When the node to be deleted has both children.
+
+In this case, we **don't need the parent node** because for this case, the node's value will be replaced by the **minValue of the right subtree** and that'll happen whether the node is root or intermediate node.
+
+Note: MinValue of SubTree is the **value of left most node**
+
+**Case 2:** When the node has only one child and **has Parent**
+  * If the node has left child only
+    * Find the subtree of the parent where the node belongs to and assign `parent.[subtree = left or right] = node.left`
+  * If the node has right child only
+    * Find the subtree of the parent where the node belongs to and assign `parent.[subtree = left or right] = node.right`
+
+**Case 3:** When the node has only one child and **has no Parent** --> it's a ROOT node
+  * If the node has left child only
+    * node.right = node.left.right
+    * node.left = node.left.left 
+  * If the node has right child only
+    * node.left = node.right.left
+    * node.right = node.right.right
+
+**Case 4:** When the node is the ROOT node and no child
+
+This is the **extreme edge case,** and it depends on the interviewer. If he wants to prohibit deleting a node if it's the only node, just return it.
+Else, remove the ROOT node and return null.
--- Binary Trees/BSTConstruction/index.ts
+++ Binary Trees/BSTConstruction/index.ts
@@ -1,93 +1,98 @@
-class BST {
+class BSTNode {
   value: number;
-  left: BST | null;
-  right: BST | null;
+  left?: BSTNode;
+  right?: BSTNode;
 
   constructor(value: number) {
     this.value = value;
-    this.left = null;
-    this.right = null;
   }
-
-  insert(value: number): BST {
+  // insert operation
+  insert(value: number) {
     if (value < this.value) {
-      if (this.left === null) {
-        this.left = new BST(value);
+      if (!this.left) {
+        this.left = new BSTNode(value);
       } else {
-        this.left.insert(value);
+        this.left?.insert(value);
       }
     } else {
-      if (this.right === null) {
-        this.right = new BST(value);
+      if (!this.right) {
+        this.right = new BSTNode(value);
       } else {
-        this.right.insert(value);
+        this.right?.insert(value);
       }
     }
     return this;
   }
-
-  contains(value: number) {
-    let currentNode: BST = this;
-    while (true) {
-      // check whether the current node value is same as the 'value'
-      if (currentNode.value === value) {
-        return true;
-      }
-      if (value < currentNode.value) {
-        // check the presence of left node
-        if (currentNode.left === null) {
-          return false;
-        }
-        currentNode = currentNode.left;
+  // contains/find operation
+  contains(value: number): boolean {
+    if (value === this.value) {
+      return true;
+    } else if (value < this.value) {
+      if (!this.left) {
+        return false;
       } else {
-        // check the presence of right node
-        if (currentNode.right === null) {
-          return false;
-        }
-        currentNode = currentNode.right;
+        return this.left?.contains(value);
+      }
+    } else {
+      if (!this.right) {
+        return false;
+      } else {
+        return this.right?.contains(value);
       }
     }
   }
-  getMinValue(): number {
-    if (this.left === null) {
-      return this.value;
-    } else {
-      return this.left.getMinValue();
+  findMinValue() {
+    let currentNode: BSTNode | null = this;
+    while (currentNode.left) {
+      currentNode = currentNode.left;
     }
+    return currentNode.value;
   }
-  remove(value: number, parent: BST | null = null): BST | null {
+  // remove/delete operation
+  remove(value: number, parent?: BSTNode) {
     if (value < this.value) {
-      if (this.left !== null) {
+      if (this.left) {
         this.left.remove(value, this);
       }
     } else if (value > this.value) {
-      if (this.right !== null) {
+      if (this.right) {
         this.right.remove(value, this);
       }
     } else {
-      // node to be deleted is found
+      // found the node to be deleted
 
-      // case 1: node has both children
-      if (this.left !== null && this.right !== null) {
-        this.value = this.right.getMinValue();
-        this.right.remove(this.value, this);
-      } else if (parent === null) {
-        if (this.left !== null) {
-          this.value = this.left.value;
-          this.right = this.left.right;
-          this.left = this.left.left;
-        } else if (this.right !== null) {
-          this.value = this.right.value;
-          this.left = this.right.left;
-          this.right = this.right.right;
+      // case 1: when the node has both children
+      if (this.left && this.right) {
+        const minRightValue = this.right?.findMinValue();
+        this.value = minRightValue;
+        this.right.remove(minRightValue, this.right);
+      } else {
+        // parent node is required from here!
+        if (parent) {
+          // case 2: when the node has only one child, and it has a parent
+          if (this.left) {
+            if (parent.left === this) {
+              parent.left = this.left;
+            } else {
+              parent.right = this.left;
+            }
+          }
+        } else {
+          // case 3: when the node is ROOT and has only one child, and it *doesn't* have a parent --> ROOT node
+          if (this.left) {
+            this.value = this.left.value;
+            this.right = this.left.right;
+            this.left = this.left.left;
+          } else if (this.right) {
+            this.value = this.right.value;
+            this.left = this.right.left;
+            this.right = this.right.right;
+          } else {
+            // ROOT node with no children
+            // case 4: When the node is ROOT and doesn't have a child
+          }
         }
-      } else if (parent.left === this) {
-        parent.left = this.left !== null ? this.left : this.right;
-      } else if (parent.right === this) {
-        parent.right = this.left !== null ? this.left : this.right;
       }
     }
-
-    return this;
   }
 }
