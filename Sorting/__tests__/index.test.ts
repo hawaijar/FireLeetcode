d2fd2d5755b93b9bdbc12f327dcb215c0e15e9bc
hawaijar <hawaijar.js@gmail.com>
2022-08-21 12:27:21 +0530

    chore: added Merge Sort Algorithm

--- Sorting/__tests__/index.test.ts
+++ Sorting/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { quickSort } from "../index";
+import { quickSort, mergeSort } from "../index";
 
 describe("QuickSort Testing", () => {
   it("should sort array[1,3,4,5,2,1]", () => {
@@ -17,6 +17,26 @@ describe("QuickSort Testing", () => {
     const array = [-5, -4, -13, -2, -1, 10];
     const sortedArray = array.sort((a, b) => a - b);
     quickSort(array);
+    expect(array).toEqual(sortedArray);
+  });
+});
+describe("MergeSort Testing", () => {
+  it("should sort array[1,3,4,5,2,1]", () => {
+    const array = [1, 3, 4, 5, 2, 1];
+    const sortedArray = array.sort((a, b) => a - b);
+    mergeSort(array);
+    expect(array).toEqual(sortedArray);
+  });
+  it("should sort array[5, 4, 3, 2, 1, 0]", () => {
+    const array = [5, 4, 3, 2, 1, 0];
+    const sortedArray = array.sort((a, b) => a - b);
+    mergeSort(array);
+    expect(array).toEqual(sortedArray);
+  });
+  it("should sort array[-5, -4, -13, -2, -1, 10]", () => {
+    const array = [-5, -4, -13, -2, -1, 10];
+    const sortedArray = array.sort((a, b) => a - b);
+    mergeSort(array);
     expect(array).toEqual(sortedArray);
   });
 });
--- Sorting/index.ts
+++ Sorting/index.ts
@@ -1,7 +1,8 @@
 function swap(array: number[], i: number, j: number) {
   [array[i], array[j]] = [array[j], array[i]];
 }
-function partition(array: number[], low: number, high: number): number {
+// One way of doing partition (left to right scanning)
+function partition1(array: number[], low: number, high: number): number {
   let index = low - 1; // always points to the last element <= pivot
   const pivotIndex = high;
   const pivot = array[pivotIndex];
@@ -19,6 +20,31 @@ function partition(array: number[], low: number, high: number): number {
   return index;
 }
 
+// Both sides scanning
+function partition(array: number[], low: number, high: number): number {
+  let [i, j] = [low - 1, high];
+  const pivotIndex = 0;
+  const pivot = array[pivotIndex];
+
+  while (i < j) {
+    // Move i till it finds an element > pivot
+    do {
+      i += 1;
+    } while (array[i] <= pivot);
+    // Move j till it finds an element < pivot
+    do {
+      j -= 1;
+    } while (array[j] > pivot);
+
+    // swap elements at 'i' and 'j' positions.
+    swap(array, i, j);
+  }
+  // finally swap the pivot and the element[j]
+  swap(array, pivotIndex, j);
+
+  return j;
+}
+
 function qs(array: number[], low: number, high: number) {
   // base case
   if (low >= high) {
@@ -33,8 +59,38 @@ export function quickSort(array: number[]) {
   qs(array, 0, array.length - 1);
 }
 
-// const a = [15, 4, 3, 2, 1, 10, 0, -1];
-// console.log("Before sorting:", a);
-// qSort(a);
-//
-// console.log("After sorting:", a);
+function merge(array: number[], low: number, mid: number, high: number) {
+  let sortedArray: number[] = [];
+  let [i, j, k] = [low, mid + 1, low];
+  while (i <= mid && j <= high) {
+    if (array[i] <= array[j]) {
+      sortedArray[k++] = array[i++];
+    } else {
+      sortedArray[k++] = array[j++];
+    }
+  }
+  // copy any remaining elements in either array1 or array2
+  while (i <= mid) {
+    sortedArray[k++] = array[i++];
+  }
+  while (j <= high) {
+    sortedArray[k++] = array[j++];
+  }
+  // finally copy all elements of sortedArray to the original array
+  for (let i = low; i <= high; i++) {
+    array[i] = sortedArray[i];
+  }
+}
+
+export function mergeSort(array: number[]) {
+  mSort(array, 0, array.length - 1);
+  function mSort(array: number[], low: number, high: number) {
+    if (low < high) {
+      const middle = Math.floor((low + high) / 2);
+      mSort(array, low, middle);
+      mSort(array, middle + 1, high);
+
+      merge(array, low, middle, high);
+    }
+  }
+}
