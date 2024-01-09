c022e4938c34df38b28de486657fefbfd9ae4d3e
Hawaijar <hawaijar.js@gmail.com>
2024-01-09 20:28:46 +0530

    chore: Added Linkedlist implementation

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import LinkedList.List;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new List();
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        System.out.println(Arrays.toString(list.print()));
+        list.remove(3);
+        System.out.println(Arrays.toString(list.print()));
+    }
+}
---
+++ src/test/java/LinkedList/ListTest.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class ListTest {
+    @Test
+    void testing() {
+        List list = new List();
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        assertThat(list.print()).hasSameSizeAs(new int[]{12,13,14,15});
+        assertThat(list.print()).contains(15,12,13,14);
+        assertThat(list.print()).containsExactly(12,13,14,15);
+        list.remove(2);
+        assertThat(list.print()).containsExactly(12,13,15);
+    }
+}
