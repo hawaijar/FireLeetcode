9a1b53fbf2b7893a83ac8b8f8d04643696c48e0a
Hawaijar <hawaijar.js@gmail.com>
2024-02-09 10:45:33 +0530

    chore: Added serial/deseralization of nested objects

--- src/main/java/Advanced/Utilities/Jackson/Demo.java
+++ src/main/java/Advanced/Utilities/Jackson/Demo.java
@@ -1,26 +1,27 @@
 package Advanced.Utilities.Jackson;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class Demo {
-	public static void main(String[] args) throws IOException {
-		// Serialization
-		List<Employee> employees = List.of(
-				new Employee("John", 30, "HR"),
-				new Employee("Jane", 25, "IT")
-		);
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(employees);
-		System.out.println(json);
-
-		// Deserialization
-		List<Employee> employees2 = mapper.readValue(new File("src/main/resources/employees.json"),
-				mapper.getTypeFactory().constructCollectionType(List.class, Employee.class));
-		System.out.println(employees2.get(6));
+	public static void seriaDeserialnestTedObjects() {
+		Car car = new Car("Ford");
+		Engine engine = new Engine("V8", 500);
+		car.setEngine(engine);
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			// Serialize Java object to JSON file
+			objectMapper.writeValue(new File("src/main/resources/car.json"), car);
+			// Serialize Java object to JSON string
+			// Deserialize JSON file to Java object
+			Car carFromFile = objectMapper.readValue(new File("src/main/resources/car2.json"), Car.class);
+			System.out.println(carFromFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		seriaDeserialnestTedObjects();
 	}
 }
